1cbf99bc678cab8d80732845dc4724a17a6549dd
arnaud longueval <arnaud.longueval@edhec.com>
2017-03-06 12:24:11 +0100

    ça ne pète plus

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,10 +2,17 @@ class OrdersController < ApplicationController
   def create
 
   # order  = Order.create!(player_sku: params[:order][:player_sku], amount: params[:order][:amount_cents].to_i, state: 'pending', email:params[:order][:eamil])
-  order  = Order.create!(order_params)
-  order.state = "pending"
-  order.save
-  redirect_to new_order_payment_path(order)
+  @order  = Order.new(order_params)
+  @order.state = "pending"
+
+
+  if @order.save
+      redirect_to new_order_payment_path(@order)
+    else
+      flash[:error] = "L'ensemble des champs doivent être renseignés (chiffres ronds et positifs)"
+      redirect_to root_path
+
+    end
 
   end
 
